a0ba48e7bec2e3e5ea97be0b2a243f32879c9678
CrazzzyRamen <itkevinwxy@gmail.com>
2018-11-03 18:17:09 -0400

    Add member account operation

---
+++ MongoRoomieDLL/MongoRoomieDLL/BusinessDAO/MemberAccountInfoDAO.cs
@@ -0,0 +1,191 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoRoomieDLL.BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoRoomieDLL.BusinessDAO
+{
+    public class MemberAccountInfoDAO : DAO<MemberAccountInfo>
+    {
+        private string clusterName = "MemberAccountInfo";
+
+        public MemberAccountInfoDAO()
+        {
+            collection = databaseMongo.GetCollection<MemberAccountInfo>(clusterName);
+        }
+
+        #region insert / update / delete
+        public void CreateMemberAccount(MemberAccountInfo mai)
+        {
+            collection.InsertOne(mai);
+        }
+
+        public long UpdateMemberAccount(MemberAccountInfo mai)
+        {
+            var result = collection.UpdateOne(Builders<MemberAccountInfo>.Filter.Eq("_id", mai._id), Builders<MemberAccountInfo>.Update
+                                            .Set("user_name", mai.user_name)
+                                            .Set("_email", mai._email)
+                                            .Set("_birthday", mai._birthday)
+                                            .Set("_cell", mai._cell)
+                                            .Set("update_day", mai.update_day)
+                                            .Set("smoke_habit", mai.smoke_habit)
+                                            .Set("has_pet", mai.has_pet)
+                                            .Set("_gender", mai._gender)
+                                            .Set("current_status", mai.current_status)
+                                            .Set("offer_place", mai.offer_place)
+                                            .Set("home_type", mai.home_type)
+                                            .Set("room_type", mai.room_type)
+                                            .Set("have_locker", mai.have_locker)
+                                            .Set("have_gym", mai.have_gym)
+                                            .Set("have_parking", mai.have_parking)
+                                            .Set("have_swim_pool", mai.have_swim_pool)
+                                            .Set("have_furniture", mai.have_furniture)
+                                            .Set("rent_fee", mai.rent_fee)
+                                            .Set("interested_place", mai.interested_place)
+                                            .Set("min_rent_fee", mai.min_rent_fee)
+                                            .Set("max_rent_fee", mai.max_rent_fee)
+                                            .Set("move_in_date", mai.move_in_date));
+
+            return result.ModifiedCount;
+        }
+
+        public long RecordLoginDay(string id, string loginDay)
+        {
+            var result = collection.UpdateOne(Builders<MemberAccountInfo>.Filter.Eq("_id", id), Builders<MemberAccountInfo>.Update.Set("login_day", loginDay));
+
+            return result.ModifiedCount;
+        }
+
+        public long ActivateAccount(string id)
+        {
+            var result = collection.UpdateOne(Builders<MemberAccountInfo>.Filter.Eq("_id", id), Builders<MemberAccountInfo>.Update.Set("account_status", "A"));
+
+            return result.ModifiedCount;
+        }
+
+        public long DeactivateAccount(string id)
+        {
+            var result = collection.UpdateOne(Builders<MemberAccountInfo>.Filter.Eq("_id", id), Builders<MemberAccountInfo>.Update.Set("account_status", "I"));
+
+            return result.ModifiedCount;
+        }
+
+        public long PendingAccount(string id)
+        {
+            var result = collection.UpdateOne(Builders<MemberAccountInfo>.Filter.Eq("_id", id), Builders<MemberAccountInfo>.Update.Set("account_status", "P"));
+
+            return result.ModifiedCount;
+        }
+
+        public long DeleteMemberAccountById(string id)
+        {
+            var result = collection.DeleteOne(Builders<MemberAccountInfo>.Filter.Eq("_id", id));
+            return result.DeletedCount;
+        }
+        #endregion
+
+        #region select
+        public List<MemberAccountInfo> GetMemberById(string id)
+        {
+            List<MemberAccountInfo> lstMember = new List<MemberAccountInfo>();
+
+            lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m._id == id).ToList();
+
+            return lstMember;
+        }
+
+        public List<MemberAccountInfo> GetActiveMemberAccountExcludeId(string id, string offerPlace, string neighbourhood, string cityName, string provinceCode, string countryName)
+        {
+            List<MemberAccountInfo> lstMember = new List<MemberAccountInfo>();
+
+            if("N".Equals(offerPlace))
+            {
+                lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m._id != id && m.account_status == "A" 
+                                            && (m.interested_place[0].interested_neighbourhood == neighbourhood
+                                            && m.interested_place[0].interested_city == cityName
+                                            && m.interested_place[0].interested_province == provinceCode
+                                            && m.interested_place[0].interested_country == countryName)).OrderBy(m => m.create_day).ToList();
+
+            }
+            else if("Y".Equals(offerPlace))
+            {
+                lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m._id != id && m.offer_place == "N" && m.account_status == "A" 
+                                            && (m.interested_place[0].interested_neighbourhood == neighbourhood
+                                            && m.interested_place[0].interested_city == cityName
+                                            && m.interested_place[0].interested_province == provinceCode
+                                            && m.interested_place[0].interested_country == countryName)).OrderBy(m => m.create_day).ToList();
+
+            }
+
+            return lstMember; 
+        }
+
+        public List<MemberAccountInfo> GetActiveMemberAccountByLocation(int count, string neighbourhood, string cityName, string provinceCode, string countryName)
+        {
+            List<MemberAccountInfo> lstMember = new List<MemberAccountInfo>();
+
+            if(count > 0)
+            {
+                lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m.account_status == "A" 
+                                            && (m.interested_place[0].interested_neighbourhood == neighbourhood
+                                            && m.interested_place[0].interested_city == cityName
+                                            && m.interested_place[0].interested_province == provinceCode
+                                            && m.interested_place[0].interested_country == countryName)).OrderBy(m => m.create_day).Take(count).ToList();
+
+            }
+            else
+            {
+                lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m.account_status == "A" 
+                                            && (m.interested_place[0].interested_neighbourhood == neighbourhood
+                                            && m.interested_place[0].interested_city == cityName
+                                            && m.interested_place[0].interested_province == provinceCode
+                                            && m.interested_place[0].interested_country == countryName)).OrderBy(m => m.create_day).ToList();
+
+            }
+
+            return lstMember;
+        }
+
+        public List<MemberAccountInfo> GetMemberAccountLoginInfo(string userName, string password)
+        {
+            List<MemberAccountInfo> lstMember = new List<MemberAccountInfo>();
+
+            lstMember = collection.AsQueryable<MemberAccountInfo>().Where(m => m.user_name == userName && m._password == password).ToList();
+
+            return lstMember;
+        }
+
+        public bool CheckUserName(string userName)
+        {
+            bool isExisted = false;
+
+            var result = collection.AsQueryable<MemberAccountInfo>().Where(m => m.user_name == userName).ToList();
+
+            if(result.Count > 0)
+            {
+                isExisted = true;
+            }
+
+            return isExisted;
+        }
+
+        public bool CheckEmail(string email)
+        {
+            bool isDuplicateEmail = false;
+
+            var result = collection.AsQueryable<MemberAccountInfo>().Where(m => m._email == email).ToList();
+
+            if(result.Count > 0)
+            {
+                isDuplicateEmail = true;
+            }
+
+            return isDuplicateEmail;
+        }
+        #endregion
+    }
+}
---
+++ MongoRoomieDLL/MongoRoomieDLL/BusinessWorkFlow/MemberAccountInfoWorkFlow.cs
@@ -0,0 +1,95 @@
+﻿using MongoRoomieDLL.BusinessDAO;
+using MongoRoomieDLL.BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoRoomieDLL.BusinessWorkFlow
+{
+    public static class MemberAccountInfoWorkFlow
+    {
+        #region insert / update / delete
+        public static void CreateMemberAccount(MemberAccountInfo mai)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            maiDAO.CreateMemberAccount(mai);
+        }
+
+        public static long UpdateMemberAccount(MemberAccountInfo mai)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.UpdateMemberAccount(mai);
+        }
+
+        public static long RecordLoginDay(string id, string loginDay)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.RecordLoginDay(id, loginDay);
+        }
+
+        public static long ActivateAccount(string id)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.ActivateAccount(id);
+        }
+
+        public static long DeactivateAccount(string id)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.DeactivateAccount(id);
+        }
+
+        public static long PendingAccount(string id)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.PendingAccount(id);
+        }
+
+        public static long DeleteMemberAccountById(string id)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.DeleteMemberAccountById(id);
+        }
+        #endregion
+
+        #region select
+        public static List<MemberAccountInfo> GetMemberById(string id)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.GetMemberById(id);
+        }
+
+        public static List<MemberAccountInfo> GetActiveMemberAccountExcludeId(string id, string offerPlace, string neighbourhood, string cityName, string provinceCode, string countryName)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.GetActiveMemberAccountExcludeId(id, offerPlace, neighbourhood, cityName, provinceCode, countryName);
+        }
+
+        public static List<MemberAccountInfo> GetActiveMemberAccountByLocation(int count, string neighbourhood, string cityName, string provinceCode, string countryName)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.GetActiveMemberAccountByLocation(count, neighbourhood, cityName, provinceCode, countryName);
+        }
+
+        public static List<MemberAccountInfo> GetMemberAccountLoginInfo(string userName, string password)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.GetMemberAccountLoginInfo(userName, password);
+        }
+
+        public static bool CheckUserName(string userName)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.CheckUserName(userName);
+        }
+
+        public static bool CheckEmail(string email)
+        {
+            MemberAccountInfoDAO maiDAO = new MemberAccountInfoDAO();
+            return maiDAO.CheckEmail(email);
+        }
+        #endregion
+    }
+}
