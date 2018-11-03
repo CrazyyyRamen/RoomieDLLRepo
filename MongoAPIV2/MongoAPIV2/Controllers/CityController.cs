d5cd5b73845789975fade22d7ed10a5bca1f72fd
CrazzzyRamen <itkevinwxy@gmail.com>
2018-11-02 22:02:03 -0400

    UPDATE API

--- MongoAPIV2/MongoAPIV2/Controllers/CityController.cs
+++ MongoAPIV2/MongoAPIV2/Controllers/CityController.cs
@@ -12,6 +12,10 @@ namespace MongoAPIV2.Controllers
     [RoutePrefix("api/city")]
     public class CityController : ApiController
     {
+        /// <summary>
+        /// Get information of city (include province and country) e.g: Toronto,ON,Canada
+        /// </summary>
+        /// <returns>a list of cities in json format</returns>
         [Route("getcity")]
         [HttpGet]
         public List<City> GetCity()
@@ -19,6 +23,11 @@ namespace MongoAPIV2.Controllers
             return CityWorkFlow.GetAllCityList();
         }
 
+        /// <summary>
+        /// Get cities that names contain keywords e.g: keyword: Hami => Hamilton
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns>a list of city with names contains keyword</returns>
         [Route("citybykeyword")]
         [HttpGet]
         public List<City> SearchCityByKeyWord(string keyword)
@@ -26,6 +35,15 @@ namespace MongoAPIV2.Controllers
             return CityWorkFlow.GetCityByKeyword(keyword);
         }
 
+        /// <summary>
+        /// Create a new city 
+        /// </summary>
+        /// <param name="cityName"></param>
+        /// <param name="provinceName"></param>
+        /// <param name="provinceCode"></param>
+        /// <param name="countryName"></param>
+        /// <param name="countryCode"></param>
+        /// <returns>http response message with city name added</returns>
         [Route("addonecity")]
         [HttpPost]
         public HttpResponseMessage AddCity(string cityName, string provinceName, string provinceCode, string countryName, string countryCode)
@@ -46,6 +64,16 @@ namespace MongoAPIV2.Controllers
             
         }
 
+        /// <summary>
+        /// Update city information
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cityName"></param>
+        /// <param name="provinceName"></param>
+        /// <param name="provinceCode"></param>
+        /// <param name="countryName"></param>
+        /// <param name="countryCode"></param>
+        /// <returns>http response message with number of cities updated and city id</returns>
         [Route("updateonecity")]
         [HttpPut]
         public HttpResponseMessage Put(string id, string cityName, string provinceName, string provinceCode, string countryName, string countryCode)
@@ -65,6 +93,11 @@ namespace MongoAPIV2.Controllers
             }
         }
 
+        /// <summary>
+        /// Delete a city by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>http response message with city id of which city was deleted</returns>
         [Route("deleteonecity")]
         [HttpDelete]
         public HttpResponseMessage DeleteCityById(string id)
