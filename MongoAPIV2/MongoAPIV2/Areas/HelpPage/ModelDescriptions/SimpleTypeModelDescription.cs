482de57fdc5256d44bddcf79e407a833b60688cc
CrazzzyRamen <itkevinwxy@gmail.com>
2018-11-11 15:03:30 -0500

    old api delete

--- MongoAPIV2/MongoAPIV2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MongoAPIV2.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- MongoAPIV2/MongoAPIV2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MongoAPIV2.WebApiApplication" Language="C#" %>
